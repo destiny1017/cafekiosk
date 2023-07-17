52a3b7ba25ca22b66005155c07b125990170f24b
KDH_Notebook <destiny1017@nate.com>
2023-07-17 22:39:12 +0900

    테스트하기 어려운 영역을 분리하기

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,11 +1,14 @@
 package sample.cafekiosk.unit;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CafeKiosk {
 
+    private static final LocalTime SHOP_OPEN_TIME = LocalTime.of(10, 0);
+    private static final LocalTime SHOP_CLOSE_TIME = LocalTime.of(22, 0);
     private final List<Beverage> beverages = new ArrayList<>();
 
     public void add(Beverage beverage) {
@@ -37,11 +40,27 @@ public class CafeKiosk {
         return totalPrice;
     }
 
-    public Order createOrder() {
-        return new Order(LocalDateTime.now(), beverages);
-    }
-
     public List<Beverage> getBeverages() {
         return this.beverages;
     }
+
+    public Order createOrder() {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalTime currentTime = currentDateTime.toLocalTime();
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
+        return new Order(currentDateTime, beverages);
+    }
+
+    public Order createOrder(LocalDateTime currentDateTime) {
+        LocalTime currentTime = currentDateTime.toLocalTime();
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("주문 시간이 아닙니다. 관리자에게 문의하세요.");
+        }
+
+        return new Order(currentDateTime, beverages);
+    }
+
 }

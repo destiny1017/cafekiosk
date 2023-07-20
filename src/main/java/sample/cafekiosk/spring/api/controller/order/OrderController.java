9b4024869e4cd268e0647788b383b816d32d659c
KDH_Notebook <destiny1017@nate.com>
2023-07-20 21:12:23 +0900

    Business Layer 테스트 : Service 테스트

---
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -0,0 +1,9 @@
+package sample.cafekiosk.spring.api.controller.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderController {
+}

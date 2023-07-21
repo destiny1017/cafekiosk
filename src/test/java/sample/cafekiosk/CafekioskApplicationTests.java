25b6cc8adf93b848d390efb4fe163730b709389a
KDH_Notebook <destiny1017@nate.com>
2023-07-21 20:17:09 +0900

    Business Layer 테스트 : 추가 요구사항 적용 테스트

--- src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
+++ src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
@@ -3,11 +3,6 @@ package sample.cafekiosk;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CafekioskApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
